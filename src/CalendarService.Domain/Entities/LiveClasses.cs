ebe51b08791e7223b25b7e84bbe3d1760eff6313
migue <migue@LAPTOP-LIQQFHEB>
2026-05-27 16:37:22 +0200

    changed name to LiveClass

--- src/CalendarService.Domain/Entities/LiveClasses.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CalendarService.Domain.Entities;
-
-public class LiveClasses(string eventId)
-{
-    public string Id { get; private set; } = Guid.NewGuid().ToString();
-    public string EventId { get; private set; } = eventId;
-
-    protected LiveClasses() : this(string.Empty) { }
-}
