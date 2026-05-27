e3770eb09aadff96192ebf37d72c5701d39ef97f
migue <migue@LAPTOP-LIQQFHEB>
2026-05-27 16:28:25 +0200

    refactor(api): rewire program startup pipeline to inject identity, persistence, and handlers
    - Added application, infrastructure persistence, and identity services setup into the WebApplicationBuilder.
    - Intercepted the HTTP pipeline to properly enforce UseAuthentication and UseAuthorization middleware.
    - Commented out HttpsRedirection to simplify local microservice testing.

--- src/CalendarService.Api/Program.cs
+++ src/CalendarService.Api/Program.cs
@@ -1,17 +1,25 @@
 using CalendarService.Api.Endpoints;
+using CalendarService.Api.Extensions;
 using CalendarService.Api.OpenApi;
 using CalendarService.Api.Security;
-
+using CalendarService.Application.DependencyInjection;
+using CalendarService.Infrastructure.Extensions.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCorsConfiguration();
 builder.Services.AddOpenApiConfiguration();
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
 
 app.UseCors("All");
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.UseAuthentication(); // "Vem är du?" (Läser JWTn)
+app.UseAuthorization();  // "Vad får du göra?" (Kollar rollerna)
 
 app.MapOpenApiEndpoints();
 app.MapCalendarEndpoints();
